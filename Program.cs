147a3e1282a078190bcb52a4e8ce38125da67271
awaclawczykatpo <a.waclawczyk@student.po.edu.pl>
2020-03-25 17:59:12 +0100

    Added unary minus operator

--- Program.cs
+++ Program.cs
@@ -4,32 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Text.RegularExpressions;
+
+
 namespace onp22
 {
     class Program
     {
-         static void Main(string[] args)
+        public static Dictionary<string, int> properDict = new Dictionary<string, int> {
+                { "-u",5 },{ "sin", 4 }, { "cos", 4 }, { "abs", 4 }, { "exp", 4 }, { "log", 4 }, {"sqrt", 4 },{"tan", 4 }, {"cosh", 4 },{"sinh", 4 },{"tanh", 4 } ,{"acos", 4 },{"asin", 4 },{"atan", 4 },
+                {"^", 3 },
+                { "*", 2 },{"/", 2 },
+                {"+", 1 },{"-", 1 },
+                { "(", 0 },{")", 0}
+
+            };
+        static void Main(string[] args)
         {
-            
+
             string equation = Console.ReadLine();
             StringBuilder equationbuilder = new StringBuilder(equation);
-            //equation integrity
-            //zalozenie, ze wprowadzamy poprawne rownania
-            //usun spacje
+            //TODO//equation integrity
+            //
+            //
 
-            //rozbij na tokeny
+            //get tokens
 
 
 
             Queue kolejka = new Queue();
             Stack stos = new Stack();
 
+
+
+            //string[] testr = Regex.Split(input, pattern);
+
+
             Dictionary<int, List<string>> priorytety = new Dictionary<int, List<string>>();
-            priorytety.Add(4, new List<string> { "abs", "cos", "exp", "log", "sin", "sqrt", "tan", "cosh", "sinh", "tanh", "acos", "asin", "atan"});
+            priorytety.Add(4, new List<string> { "abs", "cos", "exp", "log", "sin", "sqrt", "tan", "cosh", "sinh", "tanh", "acos", "asin", "atan" });
             priorytety.Add(3, new List<string> { "^" });
-            priorytety.Add(2, new List<string> { "*","/" });
-            priorytety.Add(1, new List<string> { "+","-" });
-            priorytety.Add(0, new List<string> { "(",")" });
+            priorytety.Add(2, new List<string> { "*", "/" });
+            priorytety.Add(1, new List<string> { "+", "-" });
+            priorytety.Add(0, new List<string> { "(", ")" });
             List<string> possibleTokens = new List<string> { "abs", "cos", "exp", "log", "sin", "sqrt", "tan", "cosh", "sinh", "tanh", "acos", "asin", "atan", "^", "*", "/", "+", "-", "(", ")", "x", };
 
             List<string> tokens = new List<string>();
@@ -37,26 +53,31 @@ namespace onp22
             string tmpNum = "";
             bool isNumber = false;
             bool isDouble = false;
-            foreach(char znak in equation)
+            foreach (char znak in equation)
             {
                 //build token
+                if (znak == '-' && (tokens.Count == 0 || isOperator(tokens[tokens.Count - 1])))
+                {
+                    tokens.Add("-u");
+                    continue;
 
+                }
                 if (int.TryParse(znak.ToString(), out _) && isDouble == false)
                 {
                     isNumber = true;
                     tmpNum += znak;
                 }
-                if(znak == '.')
+                if (znak == '.')
                 {
                     tmpNum += znak;
                     isDouble = true;
                 }
-                if(int.TryParse(znak.ToString(),out _)   && isDouble==true)
+                if (int.TryParse(znak.ToString(), out _) && isDouble == true)
                 {
 
                     tmpNum += znak;
                 }
-                if(!int.TryParse(znak.ToString(), out _) && isDouble && znak != '.')
+                if (!int.TryParse(znak.ToString(), out _) && isDouble && znak != '.')
                 {
                     tokens.Add(tmpNum);
                     tmpNum = "";
@@ -64,7 +85,7 @@ namespace onp22
                     isNumber = false;
                     isDouble = false;
                 }
-                if(isNumber && !(int.TryParse(znak.ToString(),out _)) && znak!='.')
+                if (isNumber && !(int.TryParse(znak.ToString(), out _)) && znak != '.')
                 {
                     tokens.Add(tmpNum);
                     tmpNum = "";
@@ -87,50 +108,64 @@ namespace onp22
             }
             if (tmpNum != "") { tokens.Add(tmpNum); }
 
-            //gotowe tokeny
+            //ready tokens
 
-            for(int i = 0; i < tokens.Count; i++)
+            for (int i = 0; i < tokens.Count; i++)
             {
                 if (tokens[i] == "(") { stos.Push(tokens[i]); continue; }
 
-                if(tokens[i] == ")")
+                if (tokens[i] == ")")
                 {
-                    while(stos.Peek().ToString() != "(" )
+                    while (stos.Peek().ToString() != "(")
                     {
                         kolejka.Enqueue(stos.Pop());
                     }
                     stos.Pop();
                     continue;
                 }
-                List<string> test = priorytety[4];
-                if(priorytety[4].Contains(tokens[i]) || priorytety[3].Contains(tokens[i]) || priorytety[2].Contains(tokens[i]) || priorytety[1].Contains(tokens[i]) || priorytety[0].Contains(tokens[i]))
-                {
-                    int pkey=0;
-                    int stoskey=0;
-                    for(int j = 0; j < priorytety.Count; j++)
-                    {
-                        if (priorytety[j].Contains(tokens[i])) { pkey = j; }
-                    }
-                    for(int j = 0;j < priorytety.Count && stos.Count>0; j++)
-                    {
-                        if (priorytety[j].Contains(stos.Peek())) stoskey = j;
-                    }
+                //if(priorytety[4].Contains(tokens[i]) || priorytety[3].Contains(tokens[i]) || priorytety[2].Contains(tokens[i]) || priorytety[1].Contains(tokens[i]) || priorytety[0].Contains(tokens[i]))
+                //{
+                //    int pkey=0;
+                //    int stoskey=0;
+                //    for(int j = 0; j < priorytety.Count; j++)
+                //    {
+                //        if (priorytety[j].Contains(tokens[i])) { pkey = j; }
+                //    }
+                //    for(int j = 0;j < priorytety.Count && stos.Count>0; j++)
+                //    {
+                //        if (priorytety[j].Contains(stos.Peek())) stoskey = j;
+                //    }
 
-                    while(stos.Count > 0 && pkey <= stoskey)
+                //    while(stos.Count > 0 && pkey <= stoskey)
+                //    {
+                //        kolejka.Enqueue(stos.Pop());
+                //        for (int j = 0; j < priorytety.Count && stos.Count > 0; j++)
+                //        {
+                //            if (priorytety[j].Contains(stos.Peek())) stoskey = j;
+                //        }
+                //    }
+                //    stos.Push(tokens[i]);
+                //    continue;
+                //}
+                if (getPriority(tokens[i]) < 10)
+                {
+
+
+                    while (stos.Count > 0 && getPriority(tokens[i]) <= getPriority(stos.Peek().ToString()))
                     {
                         kolejka.Enqueue(stos.Pop());
-                        for (int j = 0; j < priorytety.Count && stos.Count > 0; j++)
-                        {
-                            if (priorytety[j].Contains(stos.Peek())) stoskey = j;
-                        }
                     }
                     stos.Push(tokens[i]);
                     continue;
                 }
 
+
+
+
+
                 tokens[i] = tokens[i].Replace('.', ',');
-                if (int.TryParse(tokens[i].ToString(), out _) || double.TryParse(tokens[i].ToString(), out _) || float.TryParse(tokens[i].ToString(),out _) || tokens[i] == "x")
-                { kolejka.Enqueue(tokens[i]); continue;}
+                if (int.TryParse(tokens[i].ToString(), out _) || double.TryParse(tokens[i].ToString(), out _) || float.TryParse(tokens[i].ToString(), out _) || tokens[i] == "x")
+                { kolejka.Enqueue(tokens[i]); continue; }
 
 
 
@@ -146,7 +181,7 @@ namespace onp22
                 postfix[count] = kolejka.Dequeue().ToString();
                 count++;
             }
-            
+
             Console.WriteLine("Tokeny po rozbiciu w 1. fazie");
             foreach (string st in tokens)
             {
@@ -157,10 +192,128 @@ namespace onp22
             {
                 Console.Write(postfix[i] + " ");
             }
+            calcRNP(postfix, 1, 1, 10, 5);
+
+
             Console.Read();
 
 
 
         }
+        public static bool isOperator(string op)
+        {
+            switch (op)
+            {
+                case "+": return true;
+                case "-": return true;
+                case "*": return true;
+                case "/": return true;
+                case "^": return true;
+                case "(": return true;
+                default: return false;
+
+            }
+        }
+        public static int getPriority(string op)
+        {
+
+            if (properDict.TryGetValue(op, out _)) return properDict[op];
+            else return 10;
+
+        }
+        public static void findToken(string s)
+        {
+
+
+
+        }
+        public static void calcRNP(string[] tokens, double x, double x_min, double x_max, int n)
+        {
+            double dx = (x_max - x_min) / n;
+
+            for (int j = -1; j < n; j++)
+            {
+
+                Stack S = new Stack();
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    if (isNumber(tokens[i]))
+                    {
+                        S.Push(tokens[i]);
+                    }
+                    if (getPriority(tokens[i]) == 4)
+                    {
+                        double temp = double.Parse(S.Pop().ToString());
+                        S.Push(evalFun(temp, tokens[i]));
+                    }
+                    if (getPriority(tokens[i]) >= 1 && getPriority(tokens[i]) <= 3)
+                    {
+                        double a = double.Parse(S.Pop().ToString());
+                        double b = double.Parse(S.Pop().ToString());
+                        a = evalOp(a, b, tokens[i]);
+                        S.Push(a);
+                    }
+                    if (tokens[i] == "x" && j == -1)
+                    {
+                        S.Push(x);
+                    }
+                    else if (tokens[i] == "x")
+                    {
+                        S.Push(x_min);
+
+                    }
+
+                }
+
+                if (j == -1) Console.Write("\n" + x + " => " + S.Pop().ToString());
+                else Console.Write("\n" + x_min + " => " + S.Pop().ToString());
+                x_min += dx;
+            }
+
+
+        }
+        public static bool isNumber(string token)
+        {
+
+            bool flag = double.TryParse(token, out _);
+            return flag;
+        }
+        public static double evalFun(double a, string fun)
+        {
+            switch (fun)
+            {
+                case "-u": return -1 * a;
+                case "sin": return Math.Sin(a);
+                case "cos": return Math.Cos(a);
+                case "sinh": return Math.Sinh(a);
+                case "cosh": return Math.Cosh(a);
+                case "abs": return Math.Abs(a);
+                case "exp": return Math.Exp(a);
+                case "log": return Math.Log(a);
+                case "sqrt": return Math.Sqrt(a);
+                case "tan": return Math.Tan(a);
+                case "tanh": return Math.Tanh(a);
+                case "acos": return Math.Acos(a);
+                case "asin": return Math.Asin(a);
+                case "atan": return Math.Atan(a);
+            }
+
+
+            return -3.69420;
+        }
+        public static double evalOp(double a, double b, string op)
+        {
+            switch (op)
+            {
+                case "+": return a + b;
+                case "-": return b - a;
+                case "*": return a * b;
+                case "/": return b / a;
+                case "^": return Math.Pow(b, a);
+                default: return -3.42;
+            }
+        }
     }
+
+
 }
